68b0ff4203330485ccf8753f096e779624f0f1bc
Cedric-M <cirdec95@gmail.com>
2019-05-23 10:23:37 +0200

    Specifying the path in the @ApplicationPath annotation on the ApplicationConfig class

--- src/main/java/com/bank/paymentmgmt/facade/ApplicationConfig.java
+++ src/main/java/com/bank/paymentmgmt/facade/ApplicationConfig.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Application;
  *
  * @author ced
  */
-@javax.ws.rs.ApplicationPath("webresources")
+@javax.ws.rs.ApplicationPath("banking")
 public class ApplicationConfig extends Application {
 
     @Override
